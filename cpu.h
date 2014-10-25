2148b3c265e85661d5771524e315857eedac553d
Amandeep Gill <gilamand@seawolf.sonoma.edu>
2014-10-24 23:05:10 -0700

    rearrage cpu.h

--- cpu.h
+++ cpu.h
@@ -1,8 +1,8 @@
-#include <stdint.h>
-#include "defs.h"
-
 #ifndef CPU_H
 #define CPU_H
+
+#include <stdint.h>
+#include "defs.h"
 
 // spool up the cpu, create ram and load the program
 extern bool init_cpu(unsigned len, uint32_t *program);
